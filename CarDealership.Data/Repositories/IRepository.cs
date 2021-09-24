8458eb480600690cd126502658839b2e8528d25f
spitum <53660198+spitum@users.noreply.github.com>
2021-09-24 14:25:39 -0600

    Added IRepository

---
+++ CarDealership.Data/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Data.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class, new()
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task<TEntity> GetById(int Id);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
